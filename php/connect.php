273a332a9d99d061a0bf25c6245506881f374f28
Tadapong Sutthikitrungroj <dewtadapong@gmail.com>
2023-02-18 15:03:25 +0700

    edit

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <?php
- // connect to mysql                             name database
-$connect = mysqli_connect('localhost','root','','dewweb');
+ // connect to mysql                       user                   pass                 name database
+$connect = mysqli_connect('localhost','id20323391_dewtadapong','/e3=1>JI_w+EHP@m','id20323391_dewweb');
 if(mysqli_connect_error($connect)){
     echo 'Error to connect';
 }
