f178afa6a193dae0cfacd4e4ee1c21d7acf194b1
Tadapong Sutthikitrungroj <dewtadapong@gmail.com>
2023-02-20 14:42:09 +0700

    version localhost

--- Home.php
+++ Home.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+if(isset($_SESSION['fristname']))
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -215,12 +219,21 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">tadapong.sut@laf.co.th</span>
-                                <i class="mr-2 d-none d-lg-inline text-gray-600 small">itsupport</i>  
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                                <?php
+                                echo $_SESSION["username"]
+                                ?>    
+                                </span>
+                                <i class="mr-2 d-none d-lg-inline text-gray-600 small">
+                                <?php
+                                echo $_SESSION["department"]
+                                ?>    
+                                </i>  
 
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
+                            
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                             aria-labelledby="userDropdown">
