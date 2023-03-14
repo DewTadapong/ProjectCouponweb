26dddbd62861e31c75227b8ed0c8af2cc184df49
Tadapong Sutthikitrungroj <dewtadapong@gmail.com>
2023-03-14 09:04:15 +0700

    now

--- php/login.php
+++ php/login.php
@@ -1,38 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
-<head> <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.2/dist/sweetalert2.min.css">
+
+<head>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.2/dist/sweetalert2.min.css">
 </head>
+
 <body></body>
-<?php 
+<?php
 include 'connect.php';
 session_start();
 
-                // $_POST['remember'];  
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-                $sql="SELECT * FROM user WHERE username='$username' and password='$password'";
-                $result=mysqli_query($connect,$sql);
+// $_POST['remember'];  
+$username = $_POST['username'];
+$password = $_POST['password'];
+$sql = "SELECT * FROM user WHERE username='$username' and password='$password'";
+$result = mysqli_query($connect, $sql);
 
-                $row = mysqli_fetch_array($result);
-                if($row > 0){
-                        $_SESSION["username"]=$row['username'];
-                        $_SESSION["password"]=$row['password'];
-                        $_SESSION["fristname"]=$row['fristname'];
-                        $_SESSION["lastname"]=$row['lastname'];
-                        $_SESSION["department"]=$row['department'];
-                       // if(!empty($_POST['remember'])){
-                        //        setcookie('$username',$_POST['username'],time() + (10 ));
-                        //        setcookie('$password',$_POST['password'],time() + (10 ));
-                        //}else{
-                        //        if(isset($_COOKIE['username'])){
-                        //                setcookie('username','');
-                         //       if(isset($_COOKIE['password'])){
-                         //               setcookie('password','');
-                        //        }       }
-                       // }
-                        echo "<script>";
-                        echo "Swal.fire({
+$row = mysqli_fetch_array($result);
+if ($row > 0) {
+        $_SESSION["username"] = $row['username'];
+        $_SESSION["password"] = $row['password'];
+        $_SESSION["fristname"] = $row['fristname'];
+        $_SESSION["lastname"] = $row['lastname'];
+        $_SESSION["department"] = $row['department'];
+        if (!empty($_POST['remember'])) {
+                setcookie('$username', $_POST['username'], time() + (10 * 365 * 24 * 60 * 60));
+                setcookie('$password', $_POST['password'], time() + (10 * 365 * 24 * 60 * 60));
+        } else {
+                if (isset($_COOKIE['username'])) {
+                        setcookie('username', '');
+                        if (isset($_COOKIE['password'])) {
+                                setcookie('password', '');
+                        }
+                }
+        }
+        echo "<script>";
+        echo "Swal.fire({
                                 position: 'center',
                                 icon: 'success',
                                 title: 'Login Success',
@@ -45,11 +49,10 @@ session_start();
                            
                                 }
                         })";
-                        echo "</script>";          
-
-                }else{
-                        echo "<script>";
-                        echo "Swal.fire({
+        echo "</script>";
+} else {
+        echo "<script>";
+        echo "Swal.fire({
                                 position: 'center',
                                 icon: 'error',
                                 title: '!-Login Falls-!',
@@ -61,7 +64,8 @@ session_start();
                                         window.location.herf = '/Couponweb/index.php';
                                 }
                         })";
-                        echo "</script>";          
-                }
- ?>
+        echo "</script>";
+}
+?>
+
 </html>
